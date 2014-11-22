bf314ac5ff30e77997a911170687adf7f07fa407
Christian Roeer <christian@roeer.info>
2014-11-22 14:58:18 +0100

    Commit first skeleton
    make code compile and serialize to xml

---
+++ trxgen/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace trxgen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestRunType testRun = new TestRunType();
+            ResultsType results = new ResultsType();
+            List<UnitTestResultType> unitResults = new List<UnitTestResultType>();
+            var unitTestResult = new UnitTestResultType();
+            unitTestResult.outcome = "passed";
+            unitResults.Add(unitTestResult);
+
+            unitTestResult = new UnitTestResultType();
+            unitTestResult.outcome = "failed";
+            unitResults.Add(unitTestResult);
+
+            results.Items = unitResults.ToArray();
+            results.ItemsElementName = new ItemsChoiceType3[2];
+            results.ItemsElementName[0] = ItemsChoiceType3.UnitTestResult;
+            results.ItemsElementName[1] = ItemsChoiceType3.UnitTestResult;
+
+            List<ResultsType> resultsList = new List<ResultsType>();
+            resultsList.Add(results);
+            testRun.Items = resultsList.ToArray();
+
+            XmlSerializer x = new XmlSerializer(testRun.GetType());
+            x.Serialize(Console.Out, testRun);
+            x.Serialize(new XmlTextWriter("test.trx", Encoding.UTF8), testRun);
+
+            if (args.Length < 2)
+            {
+                Usage();
+                return;
+            }
+
+        }
+
+        static void Usage()
+        {
+            var programName = System.AppDomain.CurrentDomain.FriendlyName;
+            Console.WriteLine();
+            Console.WriteLine(programName);
+
+        }
+    }
+}
